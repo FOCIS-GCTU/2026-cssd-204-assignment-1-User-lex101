b89e33933e5c71f07a08d7bdb03f2edb9e79f36a
Elkanah Annang Ochinie <2425403301@live.gctu.edu.gh>
2026-06-01 16:18:32 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+#File: intials.py
+#Description: Print out my initials.
+#Assignment: Number 2
+#
+#Name: Ochinie Elkanah Annang
+#Student ID: 2425403301
+#Grader:Mr.Augustus Buckman
+#On my honour, Elkanah, this  programming assignment  is my own work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
+    # Print the small initials.
+    print()
+    print("...OEA")
+    print()
+    print("........OOOOOO............EEEEEEEEEEEEE.............AA..........")
+    print("......OOOOOOOO00..........EEEEEEEEEEEEE............AAAA.........")
+    print(".....OOO.......OOO........EEE.....................AA..AA........")
+    print(".....OOO.......OOO........EEE....................AA....AA.......")
+    print(".....OOO.......OOO........EEEEEEEEEEE...........AAA....AAA......")
+    print(".....OOO.......OOO........EEEEEEEEEEE...........AAAAAAAAAA......")
+    print(".....OOO.......OOO........EE....................AAAAAAAAAA......")
+    print(".....OOO.......OOO........EE....................AAA....AAA......")
+    print("......OOOOOOOOOOO...***...EEEEEEEEEEEEE...***...AAA....AAA..***.")
+    print("........OOOOOO0.....***...EEEEEEEEEEEEE...***...AAA....AAA..***.")
+    print()
 
 
 main()
